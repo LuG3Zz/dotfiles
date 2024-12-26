73563ef7c6863c5c9f08e3b9f9709edb9368d0f3
BrownLu <brownlu0911@gmail.com>
2024-12-26 18:34:33 +0800

    LazyVim
    mainly update the neovim config.

---
+++ dot_config/nvim/lua/plugins/code.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "skywind3000/asynctasks.vim",
+    dependencies = {
+      "skywind3000/asyncrun.vim",
+    },
+    keys = {
+      { "<F5>", ":AsyncTask file-run<cr>", desc = "Run current file" },
+      { "<F9>", ":AsyncTask file-build<cr>", desc = "Build current file" },
+    },
+    event = "VeryLazy",
+    config = function()
+      vim.g.asyncrun_open = 8
+      vim.g.asynctasks_term_pos = "kitty"
+    end,
+  },
+}
---
+++ dot_config/nvim/lua/plugins/others.lua
@@ -0,0 +1,27 @@
+return {
+  {
+
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      -- configuration goes here
+      lang = "cpp",
+      cn = { -- leetcode.cn
+        enabled = true,
+      },
+      image_support = true,
+    },
+  },
+  {
+    {
+      "3rd/image.nvim",
+      opts = {},
+    },
+  },
+}
