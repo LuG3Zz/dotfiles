2e4416b58fe3e4f853d480a9af40d4d413fc249e
brownlu <1403338195@qq.com>
2026-07-28 23:52:27 +0800

    fix: use direct function call for <CR> in norg buffers

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -164,7 +164,10 @@ autocmd('FileType', {
     pcall(vim.keymap.del, 'n', '<C-Space>', { buffer = true })
     pcall(vim.keymap.del, 'i', '<C-Space>', { buffer = true })
     vim.keymap.set('n', '<leader>tx', '<Plug>(neorg.qol.todo-items.todo.task-cycle)', { buffer = true, desc = '[neorg] Cycle Task' })
-    vim.keymap.set('n', '<CR>', '<Plug>(neorg.esupports.hop.hop-link)', { buffer = true, desc = '[neorg] Jump to Link' })
+    vim.keymap.set('n', '<CR>', function()
+      local hop = require('neorg.core').modules.get_module('core.esupports.hop')
+      if hop then hop.hop_link() end
+    end, { buffer = true, desc = '[neorg] Jump to Link' })
   end,
 })
 
