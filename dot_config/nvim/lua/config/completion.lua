d3fad63cf0f44b615d636f4a04ca2733994d6b1c
brownlu <1403338195@qq.com>
2026-07-29 08:57:24 +0800

    fix: replace <C-space> with <C-f> for blink.cmp trigger
    all <C-space> references removed from config (conflicts with input method)

--- dot_config/nvim/lua/config/completion.lua
+++ dot_config/nvim/lua/config/completion.lua
@@ -17,7 +17,7 @@ end
 blink.setup({
   keymap = {
     preset = 'default',
-    ['<C-space>'] = { 'show', 'hide' },
+    ['<C-f>'] = { 'show', 'hide' },
     ['<C-n>'] = { 'select_next', 'snippet_forward' },
     ['<C-p>'] = { 'select_prev', 'snippet_backward' },
     ['<CR>'] = { 'accept', 'fallback' },
