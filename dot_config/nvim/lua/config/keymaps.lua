c204fd0b2c8479d0d160aaa0e64cc4ddcc3e13b1
brownlu <1403338195@qq.com>
2026-07-29 15:45:54 +0800

    fix: switch back to previous window before opening journal from calendar

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -257,10 +257,12 @@ end, { desc = "Neorg: Follow link" })
 map("n", "<leader>nc", function()
   local cal = require("neorg.core").modules.get_module("core.ui.calendar")
   local ui = require("neorg.core").modules.get_module("core.ui")
+  local prev_win = vim.api.nvim_get_current_win()
   local buf, win = ui.create_split("neorg-calendar", {}, 14)
   cal.create_calendar(buf, win, {
     mode = "select_date",
     callback = function(date)
+      vim.api.nvim_set_current_win(prev_win)
       local journal = require("neorg.core").modules.get_module("core.journal")
       if journal then journal.open_diary(os.time(date), nil) end
     end,
